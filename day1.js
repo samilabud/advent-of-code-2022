0a0f5399f8ed0df9ab42cb3b638ac01841109a25
sabud <sabud@pinterest.com>
2022-12-01 20:55:38 -0400

    Joining part 1 and 2

--- day1.js
+++ day1.js
@@ -1,20 +1,4 @@
-//https://adventofcode.com/2022/day/1/input
-
-//// Part 1
-
-const fetchURL = 'https://adventofcode.com/2022/day/1/input';
-
-const resolveFn = async () => {
-  const fetchData = await fetch(fetchURL);
-  const textResult = await fetchData.text();
-  const elfs = textResult.split('\n\n');
-  const elfsCalories = elfs.map(food=>food.split('\n').reduce((calories, accum)=>Number(calories)+Number(accum)));
-  console.log(Math.max(...elfsCalories))
-}
-
-resolveFn();
-
-//// Part 2
+//Executed at https://adventofcode.com/2022/day/1/input using the chrome console
 
 const fetchURL = 'https://adventofcode.com/2022/day/1/input';
 
@@ -23,18 +7,20 @@ const resolveFn = async () => {
   const textResult = await fetchData.text();
   const elfs = textResult.split('\n\n');
   const elfsCalories = elfs.map(food=>Number(food.split('\n').reduce((calories, accum)=>Number(calories)+Number(accum))));
-  const top3Elfs = [];
+  //// Part 1
+  console.log('Part one answer', Math.max(...elfsCalories))
 
+  const top3Elfs = [];
   for(let i=0;i<=2;i++){
     const currentTopElf = Math.max(...elfsCalories);
-    
     const indexToRemove = elfsCalories.findIndex(val=>val===currentTopElf);
     if(indexToRemove>=0){
       elfsCalories.splice(indexToRemove,1);
       top3Elfs.push(currentTopElf);
     }
   }
-  console.log(top3Elfs.reduce((current,accum)=>current+accum));
+  //// Part 2
+  console.log('Part two answer',top3Elfs.reduce((current,accum)=>current+accum));
 }
 
 resolveFn();
